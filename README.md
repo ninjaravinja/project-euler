77a1b7d56caba0be27dcbc3c34471cfdec37b89f
ninjaravinja <ninja.ravinja@gmail.com>
2025-10-03 18:37:12 +0100

    Problem 22
    Completed problem 22
    Updated count in README

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ This repository contains my solutions to Project Euler problems
 I solved problems 1-11, 13, 14, 16, 20, 24 & 25, but do not have my solutions. I might remake and upload them at some point, maybe in another language
 
 
-Current count: 26
+Current count: 27
---
+++ problem22.py
@@ -0,0 +1,22 @@
+file = open("./0022_names.txt", "r")
+
+for line in file:
+    line = line.replace("\"", "")
+
+names = line.split(",")
+
+names.sort()
+
+def name_value(name):
+    sum = 0
+    for letter in name:
+        sum += ord(letter)-64
+    
+    return sum
+
+total = 0
+
+for i in range(len(names)):
+    total += (name_value(names[i]) * (i+1))
+
+print(total)
